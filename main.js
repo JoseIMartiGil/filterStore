f4b5458eb797dc33dcd6c42372d5f02d29d981b0
JoseIMartiGil <165684119+JoseIMartiGil@users.noreply.github.com>
2024-07-12 13:17:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ import './style.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   const products = [
-      { id: 1, name: 'Air Max 1', brand: 'Nike Original', price: 120, image: '/assets/airmax1.png' },
-      { id: 2, name: 'NB 9060', brand: 'New Balance', price: 80, image: '/assets/nb9060.png' },
-      { id: 3, name: 'Nike Revolution', brand: 'Nike Original', price: 90, image: '/assets/nikerev.png' },
-      { id: 4, name: 'Forum Buckle', brand: 'Adidas Original', price: 100, image: '/assets/forumbuk.png' },
-      { id: 5, name: 'Campus', brand: 'Adidas Original', price: 150, image: '/assets/campus.png' },
-      { id: 6, name: 'NB 327', brand: 'New Balance', price: 120, image: '/assets/nb327.png' },
-      { id: 7, name: 'Dunk Low', brand: 'Nike Original', price: 100, image: '/assets/dunklow.png' },
-      { id: 8, name: 'Gazelle', brand: 'Adidas Original', price: 90, image: '/assets/gazelle.png' },
-      { id: 9, name: 'Air Max SC', brand: 'Nike Original', price: 150, image: '/assets/airmaxsc.png' },
-      { id: 10, name: 'Handball Spezial', brand: 'Adidas Original', price: 120, image: '/assets/handballspezial.png' },
-      { id: 11, name: 'Air Force', brand: 'Nike Original', price: 130, image: '/assets/airforce.png' },
-      { id: 12, name: 'NB 9060', brand: 'New Balance', price: 100, image: '/assets/nb9060red.png' },
-      { id: 13, name: 'NB 480', brand: 'New Balance', price: 150, image: '/assets/nb480.png' },
-      { id: 14, name: 'Full Force Low', brand: 'Nike Original', price: 70, image: '/assets/fullforcelow.png' },
-      { id: 15, name: 'Air Max SC', brand: 'Nike Original', price: 160, image: '/assets/airmaxscgrey.png' },
-      { id: 16, name: 'Handball Spezial', brand: 'Adidas Original', price: 140, image: '/assets/handballspezialred.png' },
+      { id: 1, name: 'Air Max 1', brand: 'Nike Original', price: 120, image: './assets/airmax1.png' },
+      { id: 2, name: 'NB 9060', brand: 'New Balance', price: 80, image: './assets/nb9060.png' },
+      { id: 3, name: 'Nike Revolution', brand: 'Nike Original', price: 90, image: './assets/nikerev.png' },
+      { id: 4, name: 'Forum Buckle', brand: 'Adidas Original', price: 100, image: './assets/forumbuk.png' },
+      { id: 5, name: 'Campus', brand: 'Adidas Original', price: 150, image: './assets/campus.png' },
+      { id: 6, name: 'NB 327', brand: 'New Balance', price: 120, image: './assets/nb327.png' },
+      { id: 7, name: 'Dunk Low', brand: 'Nike Original', price: 100, image: './assets/dunklow.png' },
+      { id: 8, name: 'Gazelle', brand: 'Adidas Original', price: 90, image: './assets/gazelle.png' },
+      { id: 9, name: 'Air Max SC', brand: 'Nike Original', price: 150, image: './assets/airmaxsc.png' },
+      { id: 10, name: 'Handball Spezial', brand: 'Adidas Original', price: 120, image: './assets/handballspezial.png' },
+      { id: 11, name: 'Air Force', brand: 'Nike Original', price: 130, image: './assets/airforce.png' },
+      { id: 12, name: 'NB 9060', brand: 'New Balance', price: 100, image: './assets/nb9060red.png' },
+      { id: 13, name: 'NB 480', brand: 'New Balance', price: 150, image: './assets/nb480.png' },
+      { id: 14, name: 'Full Force Low', brand: 'Nike Original', price: 70, image: './assets/fullforcelow.png' },
+      { id: 15, name: 'Air Max SC', brand: 'Nike Original', price: 160, image: './assets/airmaxscgrey.png' },
+      { id: 16, name: 'Handball Spezial', brand: 'Adidas Original', price: 140, image: './assets/handballspezialred.png' },
   ];
   const mainElement = document.getElementById('mainElement');
   const divImageMainElement = document.createElement('div');
