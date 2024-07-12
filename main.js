55413a863391348ffd042440c30336c4f91fb6fd
JoseIMartiGil <165684119+JoseIMartiGil@users.noreply.github.com>
2024-07-12 13:24:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,28 +2,28 @@ import './style.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   const products = [
-      { id: 1, name: 'Air Max 1', brand: 'Nike Original', price: 120, image: './assets/airmax1.png' },
-      { id: 2, name: 'NB 9060', brand: 'New Balance', price: 80, image: './assets/nb9060.png' },
-      { id: 3, name: 'Nike Revolution', brand: 'Nike Original', price: 90, image: './assets/nikerev.png' },
-      { id: 4, name: 'Forum Buckle', brand: 'Adidas Original', price: 100, image: './assets/forumbuk.png' },
-      { id: 5, name: 'Campus', brand: 'Adidas Original', price: 150, image: './assets/campus.png' },
-      { id: 6, name: 'NB 327', brand: 'New Balance', price: 120, image: './assets/nb327.png' },
-      { id: 7, name: 'Dunk Low', brand: 'Nike Original', price: 100, image: './assets/dunklow.png' },
-      { id: 8, name: 'Gazelle', brand: 'Adidas Original', price: 90, image: './assets/gazelle.png' },
-      { id: 9, name: 'Air Max SC', brand: 'Nike Original', price: 150, image: './assets/airmaxsc.png' },
-      { id: 10, name: 'Handball Spezial', brand: 'Adidas Original', price: 120, image: './assets/handballspezial.png' },
-      { id: 11, name: 'Air Force', brand: 'Nike Original', price: 130, image: './assets/airforce.png' },
-      { id: 12, name: 'NB 9060', brand: 'New Balance', price: 100, image: './assets/nb9060red.png' },
-      { id: 13, name: 'NB 480', brand: 'New Balance', price: 150, image: './assets/nb480.png' },
-      { id: 14, name: 'Full Force Low', brand: 'Nike Original', price: 70, image: './assets/fullforcelow.png' },
-      { id: 15, name: 'Air Max SC', brand: 'Nike Original', price: 160, image: './assets/airmaxscgrey.png' },
-      { id: 16, name: 'Handball Spezial', brand: 'Adidas Original', price: 140, image: './assets/handballspezialred.png' },
+      { id: 1, name: 'Air Max 1', brand: 'Nike Original', price: 120, image: '/public/airmax1.png' },
+      { id: 2, name: 'NB 9060', brand: 'New Balance', price: 80, image: '/public/nb9060.png' },
+      { id: 3, name: 'Nike Revolution', brand: 'Nike Original', price: 90, image: '/public/nikerev.png' },
+      { id: 4, name: 'Forum Buckle', brand: 'Adidas Original', price: 100, image: '/public/forumbuk.png' },
+      { id: 5, name: 'Campus', brand: 'Adidas Original', price: 150, image: '/public/campus.png' },
+      { id: 6, name: 'NB 327', brand: 'New Balance', price: 120, image: '/public/nb327.png' },
+      { id: 7, name: 'Dunk Low', brand: 'Nike Original', price: 100, image: '/public/dunklow.png' },
+      { id: 8, name: 'Gazelle', brand: 'Adidas Original', price: 90, image: '/public/gazelle.png' },
+      { id: 9, name: 'Air Max SC', brand: 'Nike Original', price: 150, image: '/public/airmaxsc.png' },
+      { id: 10, name: 'Handball Spezial', brand: 'Adidas Original', price: 120, image: '/public/handballspezial.png' },
+      { id: 11, name: 'Air Force', brand: 'Nike Original', price: 130, image: '/public/airforce.png' },
+      { id: 12, name: 'NB 9060', brand: 'New Balance', price: 100, image: '/public/nb9060red.png' },
+      { id: 13, name: 'NB 480', brand: 'New Balance', price: 150, image: '/public/nb480.png' },
+      { id: 14, name: 'Full Force Low', brand: 'Nike Original', price: 70, image: '/public/fullforcelow.png' },
+      { id: 15, name: 'Air Max SC', brand: 'Nike Original', price: 160, image: '/public/airmaxscgrey.png' },
+      { id: 16, name: 'Handball Spezial', brand: 'Adidas Original', price: 140, image: '/public/handballspezialred.png' },
   ];
   const mainElement = document.getElementById('mainElement');
   const divImageMainElement = document.createElement('div');
   divImageMainElement.id='divImageMainElement';
   const imageMainElement = document.createElement('img');
-  imageMainElement.src='./assets/main-cover.jpeg';
+  imageMainElement.src='./public/main-cover.jpeg';
   imageMainElement.alt='';
   imageMainElement.id='imageMainElement';
   divImageMainElement.appendChild(imageMainElement);
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const productsContainer = document.getElementById('productsContainer');
   const filterButton = document.getElementById('filterButton');
   const imageFilterButton = document.createElement('img');
-  imageFilterButton.src='./assets/vectorDown.png';
+  imageFilterButton.src='./public/vectorDown.png';
   imageFilterButton.alt=''
   filterButton.innerHTML = '';  // Limpiar el texto del botón si es necesario
   filterButton.appendChild(imageFilterButton); 
@@ -126,10 +126,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // Alternar la visibilidad del contenedor de productos
     if (filterModal.style.display === 'none') {
         filterModal.style.display = 'block';
-        imageFilterButton.src='./assets/vectorUp.png';
+        imageFilterButton.src='./public/vectorUp.png';
     } else {
         filterModal.style.display = 'none';
-        imageFilterButton.src='./assets/vectorDown.png';
+        imageFilterButton.src='./public/vectorDown.png';
     }
   });
 
